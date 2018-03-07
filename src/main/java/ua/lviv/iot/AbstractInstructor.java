c1743d6498002b5f954333b6b24fde488539f8e4
Zenoviy Veres <viruslviv@gmail.com>
2018-03-07 10:45:39 +0200

    Add more constrcutors

--- src/main/java/ua/lviv/iot/AbstractInstructor.java
+++ src/main/java/ua/lviv/iot/AbstractInstructor.java
@@ -8,8 +8,15 @@ public abstract class AbstractInstructor implements Instructor {
     
     private List<Student> students;
     
-    public AbstractInstructor(String courseName, List<Student> student) {
+    public AbstractInstructor() {}
+    
+    public AbstractInstructor(String courseName) {
+        this(courseName, null);
+    }
+    
+    public AbstractInstructor(String courseName, List<Student> students) {
         this.courseName = courseName;
+        this.students = students;
     }
 
 
